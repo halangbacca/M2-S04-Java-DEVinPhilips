8e4f2a3fe0d46b90f60561e63cdc7464b0ac0e32
halangbacca <halanbacca@outlook.com>
2023-02-22 16:27:02 -0300

    Protected

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int idade = 0;
+
+        do {
+            try {
+
+                System.out.print("Digite a sua idade: ");
+
+                Scanner scanner = new Scanner(System.in);
+                idade = scanner.nextInt();
+
+                if (idade < 0 || idade == 0 || idade > 100) {
+                    throw new Exception("Não foi possível determinar sua idade!");
+                }
+
+                scanner.close();
+
+                System.out.println("Olá, você tem " + idade + " anos de idade!");
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+        } while (idade < 0 || idade == 0 || idade > 100);
+
     }
 }
